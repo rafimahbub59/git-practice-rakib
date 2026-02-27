3cba966c989fd29495b0b30d3df515671239fdb9
rakibmahbub <rakibbinmahbubtalukder@gmail.com>
2026-02-27 13:03:20 +0600

    Add multiply function to calculator

--- src/main.py
+++ src/main.py
@@ -7,3 +7,7 @@ from utils import add, subtract
 
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
+
+from utils import add, subtract, multiply
+
+print("Multiplication:", multiply(5, 3))
